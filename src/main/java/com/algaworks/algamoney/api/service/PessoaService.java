e70d995b7074b732858b62f2cdbc7f5f79d506a1
Daniel Araújo <danielaraujo.fs@gmail.com>
2024-07-27 20:21:22 -0300

    Atualizando pessoas com PUT

---
+++ src/main/java/com/algaworks/algamoney/api/service/PessoaService.java
@@ -0,0 +1,27 @@
+package com.algaworks.algamoney.api.service;
+
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.stereotype.Service;
+
+import com.algaworks.algamoney.api.model.Pessoa;
+import com.algaworks.algamoney.api.repository.PessoaRepository;
+
+@Service
+public class PessoaService {
+
+	@Autowired
+	private PessoaRepository pessoaRepository;
+
+	public Pessoa atualizar(Long codigo, Pessoa pessoa) {
+		Pessoa pessoaSalva = this.pessoaRepository.findOne(codigo);
+
+		if (pessoaSalva == null) {
+			throw new EmptyResultDataAccessException(1);
+		}
+
+		BeanUtils.copyProperties(pessoa, pessoaSalva, "codigo");
+		return this.pessoaRepository.save(pessoaSalva);
+	}
+}
